4764fd56cf221cc532194ffbd162b6a485f1ad2a
benhurst1 <ben-hurst@outlook.com>
2024-04-30 14:42:47 +0100

    added search bar

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,8 +4,14 @@ import Header from "./components/Header";
 
 export default function Home() {
   const [houses, setHouses] = useState([]);
-
+  const [search, setSearch] = useState("");
   const navigate = useNavigate();
+
+  const filteredHouses = houses.filter((house) => {
+    const houseData =
+      `${house.house_number} ${house.street_address} ${house.city} ${house.postcode}`.toLowerCase();
+    return houseData.includes(search.toLowerCase());
+  });
 
   useEffect(() => {
     fetch("/api/houses")
@@ -20,26 +26,34 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="houseList">
-        {houses.length > 0 &&
-          houses.map((house) => (
-            <button
-              key={house.id}
-              className="houseBtn"
-              onClick={clickHandle(
-                house.id,
-                house.house_number,
-                house.street_address,
-                house.city,
-                house.postcode
-              )}
-            >
-              <h2>
-                {house.house_number} {house.street_address}, {house.city}
-                {house.postcode}
-              </h2>
-            </button>
-          ))}
+      <div className="homepage">
+        <input
+          type="text"
+          placeholder="Search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="houseList">
+          {filteredHouses.length > 0 &&
+            filteredHouses.map((house) => (
+              <button
+                key={house.id}
+                className="houseBtn"
+                onClick={clickHandle(
+                  house.id,
+                  house.house_number,
+                  house.street_address,
+                  house.city,
+                  house.postcode
+                )}
+              >
+                <h2>
+                  {house.house_number} {house.street_address}, {house.city}
+                  {house.postcode}
+                </h2>
+              </button>
+            ))}
+        </div>
       </div>
     </>
   );
